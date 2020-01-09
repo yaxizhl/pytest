f064d2b546f42c498b8452883ff99f722453b3e1
zhuhailiang4 <zhuhailiang@jd.com>
2020-01-09 13:07:37 +0800

    update

--- os.py
+++ os.py
@@ -1,3 +1,12 @@
 import os
 # print(os.uname())
-print(os.path.abspath('../'))
+# print(os.path.abspath('../'))
+# os.rmdir('./abc')
+import shutil
+
+# shutil.rmtree('./abc', ignore_errors=True)
+# os.mkdir('hello/as/sd')
+# os.makedirs('hello/as/sd')
+# shutil.copytree("newpos", "newpos2")
+shutil.rmtree("newpos")
+shutil.move("newpos2/as", "newpos")
