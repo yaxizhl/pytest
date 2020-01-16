06bc75a403176766e686aa8c37f7cd0ab1d7b848
zhuhailiang4 <zhuhailiang@jd.com>
2020-01-16 14:01:15 +0800

    1

--- itchat12.py
+++ itchat12.py
@@ -1,10 +1,14 @@
 import itchat
+import time
+from itchat.content import *
 
 
 @itchat.msg_register(itchat.content.TEXT)
 def print_content(msg):
-    print(msg['Text'])
+    print(msg['Text'], msg.user)
+    if msg.user.UserName == 'filehelper':
+        itchat.send('Hello, filehelper', toUserName=msg.user.UserName)
 
 
-itchat.auto_login()
+itchat.auto_login(hotReload=True)
 itchat.run()
